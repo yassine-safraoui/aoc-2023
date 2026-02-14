a228e4c13e04ad526283b48c2542342773fc0738
Yassine Safraoui <yassine.safraoui@grenoble-inp.org>
2026-02-14 16:22:13 +0100

    egui playground

---
+++ src/bin/egui_playground.rs
@@ -0,0 +1,50 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use eframe::egui;
+use egui::{Color32, Pos2, Sense, Stroke};
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let (response, painter) = ui.allocate_painter(ui.available_size(), Sense::empty());
+
+            painter.line(
+                vec![Pos2 { x: 0.0, y: 0.0 }, Pos2 { x: 100.0, y: 100.0 }],
+                Stroke::new(5.0, Color32::from_rgb(25, 200, 100)),
+            );
+        });
+    }
+}
